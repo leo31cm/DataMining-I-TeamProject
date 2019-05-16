443a70ed1beb69268c1ebaefd68f331163b3e79d
Tobias Schäfer <tobias.schaefer@media-t.de>
2019-05-16 18:00:42 +0200

    Update

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing
 
 
@@ -47,3 +48,39 @@ def get_data_experimental(standard=True, minmax=False):
         return x_train_minmax, y_train
     else:
         return False
+
+
+def f(x):
+    # define already build functions
+    # removing string data from column to make it float type
+    mapping_dict = {'8 years': 8, '10+ years': 10, '3 years': 3, '5 years': 5, '< 1 year': 0, '2 years': 2,
+                    '4 years': 4, '9 years': 9, '7 years': 7, '1 year': 1, '6 years': 6, 'n/a': np.nan}
+    try:
+        return mapping_dict[x]
+    except:
+        return x
+
+
+# Finding the median value in the respective columns
+def cs(i):
+    if i > 1000:
+        i = i / 10
+        return i
+    else:
+        return i
+
+
+# adding monthly income
+def mi(row):
+    ai = row["Annual Income"]
+    md = row["Monthly Debt"]
+
+    miv = ai / 12 - md
+    return miv
+
+
+# adding CreditRatio as new feature
+def cr(row):
+    cla = row["Current Loan Amount"]
+    ai = row["Annual Income"]
+    return cla / ai
